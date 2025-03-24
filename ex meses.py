d4ee01f4112ff5620a204fe9f5427fae206caecf
BRngDiceNAtSUKI <gabrielsilvaguti@gmail.com>
2025-03-24 12:46:39 -0300

    exercicio 10 de meses
    exercicios para meses, com função if e else e elif
    obs: tentar refazer com elif e com a formatação correta

---
+++ ex meses.py
@@ -0,0 +1,25 @@
+m = int(input())
+if m == 1:
+print('January')
+else: m == 2
+print('Febuary')
+if m == 3
+print('March')
+else: m == 4
+print('April')
+if m == 5
+print('May')
+else: m == 6
+print('June')
+if m == 7
+print('July')
+else: m == 8
+print('August')
+if m == 9
+print('September')
+else: m == 10
+print('October')
+if m == 11
+print('November')
+else: m == 12
+print('December')

bcd3f11c51f296375bed227b8b837e839d8d836d
BRngDiceNAtSUKI <gabrielsilvaguti@gmail.com>
2025-03-24 10:31:17 -0300

    exercício 5 de python
    exercícios de python,
    obs: analisar o arquivo dps

---
+++ seleção ex05.py
@@ -0,0 +1,8 @@
+letra = input('letra?  ')
+if letra == 'a' or letra == 'e' or letra == 'i' or \
+   letra == 'o' or letra == 'u' or \
+   letra == 'A' or letra == 'E' or letra == 'I' or \
+   letra == 'O' or letra == 'U' :
+      print('vogal')
+else:
+    print('não vogal')
